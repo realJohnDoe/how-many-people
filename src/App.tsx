cf2e5d90d192cfb1d047750b53c959b0121515a6
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-12-30 10:02:29 +0100

    Switch to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@
 function App() {
   return (
     <>
-      <div className="circle">1</div>
+      <div className="w-28 h-28 mx-auto my-8 bg-[#61dafbaa] rounded-full flex justify-center items-center text-white text-2xl font-bold">
+        1
+      </div>
     </>
   )
 }
