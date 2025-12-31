dcf1971eeeaac48e9d45fbef57a1288c724444fc
Johannes Döllinger <johannes.doellinger@gmail.com>
2025-12-31 13:03:19 +0100

    Factor out data

---
+++ src/data.ts
@@ -0,0 +1,100 @@
+// --- Types ---
+export type CircleData = {
+  id: number;
+  name: string;
+  numberOfPersons: number;
+  yearlyTurnOver: number; // Explicitly declared
+};
+
+// --- Constants ---
+export const circlesData: CircleData[] = [
+  { id: 1, name: "You", numberOfPersons: 1, yearlyTurnOver: 40000 },
+  { id: 2, name: "Your Family", numberOfPersons: 4, yearlyTurnOver: 90000 },
+  { id: 3, name: "Your Friends", numberOfPersons: 30, yearlyTurnOver: 1000000 },
+  { id: 4, name: "A Village", numberOfPersons: 200, yearlyTurnOver: 10000000 },
+  { id: 5, name: "Town", numberOfPersons: 10000, yearlyTurnOver: 400000000 },
+  {
+    id: 6,
+    name: "City",
+    numberOfPersons: 1000000,
+    yearlyTurnOver: 40000000000,
+  },
+  {
+    id: 7,
+    name: "Walmart",
+    numberOfPersons: 2000000,
+    yearlyTurnOver: 700000000000,
+  },
+  {
+    id: 8,
+    name: "Germany",
+    numberOfPersons: 80000000,
+    yearlyTurnOver: 5000000000000,
+  },
+  {
+    id: 9,
+    name: "Bosch",
+    numberOfPersons: 400000,
+    yearlyTurnOver: 90000000000,
+  },
+  {
+    id: 10,
+    name: "China",
+    numberOfPersons: 1000000000,
+    yearlyTurnOver: 40000000000000,
+  },
+  {
+    id: 11,
+    name: "India",
+    numberOfPersons: 1000000000,
+    yearlyTurnOver: 20000000000000,
+  },
+  {
+    id: 12,
+    name: "World",
+    numberOfPersons: 8000000000,
+    yearlyTurnOver: 100000000000000,
+  },
+  {
+    id: 13,
+    name: "Elon Musk",
+    numberOfPersons: 1,
+    yearlyTurnOver: 300000000000,
+  },
+  {
+    id: 14,
+    name: "USA",
+    numberOfPersons: 300000000,
+    yearlyTurnOver: 30000000000000,
+  },
+  {
+    id: 15,
+    name: "Amazon",
+    numberOfPersons: 2000000,
+    yearlyTurnOver: 600000000000,
+  },
+  {
+    id: 16,
+    name: "State Grid Corporation of China",
+    numberOfPersons: 1000000,
+    yearlyTurnOver: 500000000000,
+  },
+  {
+    id: 17,
+    name: "Africa",
+    numberOfPersons: 1000000000,
+    yearlyTurnOver: 3000000000000,
+  },
+  {
+    id: 18,
+    name: "Stuttgart",
+    numberOfPersons: 612000,
+    yearlyTurnOver: 59000000000,
+  },
+  {
+    id: 19,
+    name: "OpenAI",
+    numberOfPersons: 3000,
+    yearlyTurnOver: 3700000000,
+  },
+];
